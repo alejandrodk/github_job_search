0d0782bf483666944334c924a911ec2ae707a0ca
alejandrodk <ottoabarriosp@hotmail.com>
2021-02-15 19:57:17 -0300

    define job position class

---
+++ src/main/java/com/platzi/jobsearch/JobPosition.java
@@ -0,0 +1,99 @@
+package com.platzi.jobsearch;
+
+import com.google.gson.annotations.SerializedName;
+
+public class JobPosition {
+    private String id;
+    private String type;
+    private String url;
+    @SerializedName("created_at")
+    private String createdAt;
+    private String company;
+    @SerializedName("company_url")
+    private String companyUrl;
+    private String title;
+    private String location;
+    private String description;
+    @SerializedName("company_logo")
+    private String companyLogo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCompanyUrl() {
+        return companyUrl;
+    }
+
+    public void setCompanyUrl(String companyUrl) {
+        this.companyUrl = companyUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCompanyLogo() {
+        return companyLogo;
+    }
+
+    public void setCompanyLogo(String companyLogo) {
+        this.companyLogo = companyLogo;
+    }
+}
