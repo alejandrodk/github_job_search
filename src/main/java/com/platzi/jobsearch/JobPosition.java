eb439b0a80beb3b7ddff360da73e694a61203a9d
alejandrodk <ottoabarriosp@hotmail.com>
2021-02-15 21:34:13 -0300

    Add comparation methods to JobPosition class

--- src/main/java/com/platzi/jobsearch/JobPosition.java
+++ src/main/java/com/platzi/jobsearch/JobPosition.java
@@ -2,6 +2,8 @@ package com.platzi.jobsearch;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Objects;
+
 public class JobPosition {
     private String id;
     private String type;
@@ -95,5 +97,43 @@ public class JobPosition {
 
     public void setCompanyLogo(String companyLogo) {
         this.companyLogo = companyLogo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobPosition that = (JobPosition) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(type, that.type) &&
+                Objects.equals(url, that.url) &&
+                Objects.equals(createdAt, that.createdAt) &&
+                Objects.equals(company, that.company) &&
+                Objects.equals(companyUrl, that.companyUrl) &&
+                Objects.equals(title, that.title) &&
+                Objects.equals(location, that.location) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(companyLogo, that.companyLogo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type, url, createdAt, company, companyUrl, title, location, description, companyLogo);
+    }
+
+    @Override
+    public String toString() {
+        return "JobPosition{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", url='" + url + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                ", company='" + company + '\'' +
+                ", companyUrl='" + companyUrl + '\'' +
+                ", title='" + title + '\'' +
+                ", location='" + location + '\'' +
+                ", description='" + description + '\'' +
+                ", companyLogo='" + companyLogo + '\'' +
+                '}';
     }
 }
