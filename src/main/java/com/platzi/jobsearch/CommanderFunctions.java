553c0862cbd45148fed0c871b9fc3c5a07789cb0
alejandrodk <ottoabarriosp@hotmail.com>
2021-02-16 09:35:20 -0300

    Parse inputs from JCommander

--- src/main/java/com/platzi/jobsearch/CommanderFunctions.java
+++ src/main/java/com/platzi/jobsearch/CommanderFunctions.java
@@ -1,7 +1,11 @@
 package com.platzi.jobsearch;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class CommanderFunctions {
@@ -14,5 +18,21 @@ public class CommanderFunctions {
 
         jCommander.setProgramName(CLIName);
         return jCommander;
+    }
+
+    static Optional<List<Object>> parseArguments(
+            JCommander jCommander,
+            String[] args,
+            Consumer<JCommander> onError
+    ){
+        try {
+            jCommander.parse(args);
+            return Optional.of(jCommander.getObjects());
+        } catch (ParameterException exception) {
+            // catch exceptions from validator
+            onError.accept(jCommander);
+        }
+
+        return Optional.empty();
     }
 }
--- src/main/java/com/platzi/jobsearch/JobSearch.java
+++ src/main/java/com/platzi/jobsearch/JobSearch.java
@@ -3,11 +3,27 @@ package com.platzi.jobsearch;
 import com.beust.jcommander.JCommander;
 import com.platzi.jobsearch.cli.CLIArguments;
 
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import static com.platzi.jobsearch.CommanderFunctions.buildCommanderWithName;
+import static com.platzi.jobsearch.CommanderFunctions.parseArguments;
 
 public class JobSearch {
     public static void main(String[] args) {
         System.out.println("Hello Job Search!!");
         JCommander jCommander = buildCommanderWithName("job-search", CLIArguments::newInstance);
+
+        Stream<CLIArguments> streamOfCLI =
+                parseArguments(jCommander, args, JCommander::usage)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(obj -> (CLIArguments) obj);
+
+        Optional<CLIArguments> cliArgumentsOptional =
+                streamOfCLI.filter(cli -> !cli.isHelp())
+                .filter(cli -> cli.getKeyword() != null)
+                .findFirst();
     }
 }
